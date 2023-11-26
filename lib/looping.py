082d8b514dfaeb48db8f1754142f900240b1e908
Ahmed yussuf Abdullahi <ahmedyussuf917@gmail.com>
2023-11-26 15:56:27 +0300

    completed

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,28 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+     count = 10
+     while count >= 1:
+        print(count)
+        count -= 1
+     print("Happy New Year!")
+    
 
 def square_integers(int_list):
     # code goes here!
+    squared_numbers = [num ** 2 for num in int_list]
+    return squared_numbers
     pass
 
 def fizzbuzz():
     # code goes here!
-    pass
+      for num in range(1, 101):
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        pass
